572a212250f115b8f0801e25f64244bcaad06567
Lan Sizhong <lsz@nzqrc.cn>
2024-04-12 22:00:43 +0800

    plugin project setttings

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -66,4 +66,31 @@ return {
     -- run the lua command to install: function() vim.fn["mkdp#util#install"]()
     build = ":call mkdp#util#install()",
   },
+  {
+    "ahmedkhalf/project.nvim",
+    config = function()
+      require("project_nvim").setup {
+        active = true,
+        on_config_done = nil,
+        manual_mode = false,
+        detection_methods = { "pattern" },
+        patterns = {
+          ".git",
+          "_darcs",
+          ".hg",
+          ".bzr",
+          ".svn",
+          -- "Makefile",
+          "package.json",
+        },
+        exclude_dirs = {},
+        show_hidden = false,
+        silent_chdir = true,
+        scope_chdir = "global",
+        ignore_lsp = {},
+        datapath = vim.fn.stdpath "data",
+      }
+    end,
+    event = "VeryLazy",
+  },
 }
