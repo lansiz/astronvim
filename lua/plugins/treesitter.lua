8db5ff8c2c433d5095166269590985c1c91e5c48
Lan Sizhong <lsz@nzqrc.cn>
2024-04-13 14:50:54 +0800

    use community pack cmake

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,6 @@ return {
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "make",
-      "cmake",
       "vim",
       "regex",
       "diff",
